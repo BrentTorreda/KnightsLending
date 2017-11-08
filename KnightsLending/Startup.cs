1139abec710378868bba1dd7d1d42fc28cbd3225
Brent Raymund Torreda <Brent Raymund Torreda>
2017-11-09 09:28:26 +1100

    Add project files.

---
+++ KnightsLending/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KnightsLending.Startup))]
+namespace KnightsLending
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
